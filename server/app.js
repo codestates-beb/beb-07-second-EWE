c2696f532d9f8d7355bf7c254f8314fe0eed94ce
HyunTae <113375908+hyuntae384@users.noreply.github.com>
2023-01-02 15:03:02 +0900

    서버 개발 환경 세팅

---
+++ server/app.js
@@ -0,0 +1,5 @@
+const express = require("express");
+const app = express();
+const routes = require("./routes");
+
+app.use("/", routes);
---
+++ server/controllers/user.controller.js
@@ -0,0 +1,5 @@
+module.exports = {
+  user_join_post : async (req, res) => {},
+  user_login_post : async (req, res) => {},
+  user_transfer_post : async (req, res) => {}
+};
