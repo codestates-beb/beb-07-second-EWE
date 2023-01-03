912e28bfae09caa325c36c49cccd7262df425e5a
Russ <yunsubin481@gmail.com>
2023-01-03 09:09:36 +0900

    SignupPage.js validation, Header.js, MainPage.js update

--- client/src/components/Post.js
+++ client/src/components/Post.js
@@ -1,6 +1,16 @@
 const Post = () => {
     return(
-        <div>Post</div>
+        <div className="post_container">
+            <div className="user_img"></div>
+            <div className="post_title"></div>
+            <div className="creator"></div>
+            <div className="view"></div>
+            <div className="post_num"></div>
+            <div className="createdAt"></div>
+            <div className="image"></div>
+            <div className="contents"></div>
+            <div className="comments"></div>
+        </div>
     );
 }
 export default Post;
--- client/src/pages/MainPage.js
+++ client/src/pages/MainPage.js
@@ -1,7 +1,19 @@
 import '../assets/css/main.css'
+import Footer from '../components/Footer'
+import Post from '../components/Post';
 const MainPage = () => {
     return(
-        <div>MainPage</div>
+        <div>
+            <a href='/writepage' className='write'>
+                <div className='post_button'>POST</div>
+                
+            </a>
+            <div className='category'></div>
+            <div className='post_list'>
+                <Post/>
+            </div>
+            <Footer/>
+        </div>
     );
 }
 export default MainPage;
