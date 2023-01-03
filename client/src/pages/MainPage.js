ee244cda9dfb893f29327666fe6ca8df1e4daec3
Russ <yunsubin481@gmail.com>
2023-01-04 00:06:38 +0900

    MainPage.js 구현

--- client/src/pages/MainPage.js
+++ client/src/pages/MainPage.js
@@ -1,7 +1,19 @@
+import { useState, useEffect } from 'react'
 import '../assets/css/main.css'
 import Footer from '../components/Footer'
-import PostList from '../components/PostList';
+import PostList from '../components/PostList'
+import {getPosts} from '../apis/post'
 const MainPage = () => {
+    const [posts, setPosts] = useState([])
+
+
+    useEffect(()=>{
+        getPosts()
+        .then((result)=>{
+            setPosts(result)
+        })
+    },[])
+
     return(
         <div>
             <a href='/writepage' className='write'>
@@ -9,11 +21,13 @@ const MainPage = () => {
                 
             </a>
             <div className='category'>
-                <i className="fa-solid fa-turkey"></i>
-                <i className="fa-solid fa-bowl-chopsticks-noodles"></i>
+            <a href='/'><h2>Korean</h2></a>
+            <a href='/'><h2>Western</h2></a>
+            <a href='/'><h2>Japanese</h2></a>
+            <a href='/'><h2>Chinese</h2></a>
             </div>
             <div className='post_list'>
-                <PostList/>
+                <PostList key={posts.id} posts={posts}/>
             </div>
             <div className='pagination'>
 
