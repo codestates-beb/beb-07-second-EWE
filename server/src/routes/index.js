f4a4e19e63fadbc340412efe1ef6819b2d58fab7
HyunTae <113375908+hyuntae384@users.noreply.github.com>
2023-01-04 22:25:49 +0900

    add: users api draft

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -2,12 +2,4 @@ const express = require('express');
 
 const router = express.Router();
 
-// const main = require('./main.route');
-// const user = require('./user.route');
-// const post = require('./post.route');
-
-// router.use('/', main);
-// router.use('/user', user);
-// router.use('/post', post);
-
 module.exports = router;
