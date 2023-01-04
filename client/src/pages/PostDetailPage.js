7272b91b14947ffc39254f38396fec14cac191e3
Duhoon <eclip6@naver.com>
2023-01-04 15:32:22 +0900

    feat: 포스트 디테일 페이지 UI 구현 디테일 페이지의 UI 구현은 완료되었지만 원래 추가되기로 했던 장소 부분이 아직 API 연동 문제로 완료되지 않았습니다. 추후 수정이 될 예정입니다.

--- client/src/pages/PostDetailPage.js
+++ client/src/pages/PostDetailPage.js
@@ -9,6 +9,7 @@ import {Swiper, SwiperSlide} from "swiper/react";
 // css
 import "../assets/css/postdetail.css";
 import "swiper/css";
+import axios from "axios";
 
 function convertDate(date){
     const parsingDate = new Date(date).toLocaleString();
@@ -22,6 +23,12 @@ const PostDetailPage = () => {
         (async()=>{
             const result = await getPostOne(1);
             setPost(result);
+
+            const location = await axios.get("https://map.kakao.com/?itemId=1914659693")
+            .then(result=>result)
+            .catch(error=>error);
+
+            console.log(location);
         })();
     }, []);
 
@@ -30,49 +37,57 @@ const PostDetailPage = () => {
     return(
         <div className="container">
             {post ?
-            <div className="detail_wrapper">
-                <div className="detail_header">
-                    <div className="header_section">
-                        <div className="detail_profile_wrapper">
-                            <div className="profile_frame">
-                                <img src="https://spnimage.edaily.co.kr/images/Photo/files/NP/S/2022/07/PS22072100041.jpg"/>
+            <div className="detail_section">
+                <div className="detail_wrapper">
+                    <div className="detail_header">
+                        <div className="header_section">
+                            <div className="detail_profile_wrapper">
+                                <div className="profile_frame">
+                                    <img src="https://spnimage.edaily.co.kr/images/Photo/files/NP/S/2022/07/PS22072100041.jpg"/>
+                                </div>
+                            </div>
+                            <div className="detail_title_wrapper">
+                                <h1>{post.title}</h1>
+                                <p>
+                                    <i class="fas fa-pen"/>
+                                    <span>{post.creator}</span>
+                                    <span>|</span>
+                                    <i class="fas fa-eye" />
+                                    <span>{post.views}</span>
+                                </p>
                             </div>
                         </div>
-                        <div className="detail_title_wrapper">
-                            <h1>{post.title}</h1>
-                            <p>{`작성자: ${post.creator} | 조회 수: ${post.views}`}</p>
+                        <div className="detail_data_wrapper">
+                            <p># {post.id}</p>
+                            <p>{convertDate(post.created_at)}</p>
                         </div>
                     </div>
-                    <div className="detail_data_wrapper">
-                        <p>{post.id}</p>
-                        <p>{convertDate(post.created_at)}</p>
+                    <div className="detail_image_wrapper">
+                        <Swiper
+                            slidesPerView={1}
+                        >
+                            <SwiperSlide>
+                                <div className="swiper_image_wrapper">
+                                    <img src={post.images}/>
+                                </div>
+                            </SwiperSlide>
+                            <SwiperSlide>
+                                <div className="swiper_image_wrapper">
+                                    <img src={post.images}/>
+                                </div>
+                            </SwiperSlide>
+                            <SwiperSlide>
+                                <div className="swiper_image_wrapper">
+                                    <img src={post.images}/>
+                                </div>
+                            </SwiperSlide>                        
+                        </Swiper>
                     </div>
+                    <div className="detail_content_wrapper">
+                        <p>{post.content}</p>
+                    </div>
+                    
                 </div>
-                <div className="detail_image_wrapper">
-                    <Swiper
-                        slidesPerView={1}
-                    >
-                        <SwiperSlide>
-                            <div className="swiper_image_wrapper">
-                                <img src={post.images}/>
-                            </div>
-                        </SwiperSlide>
-                        <SwiperSlide>
-                            <div className="swiper_image_wrapper">
-                                <img src={post.images}/>
-                            </div>
-                        </SwiperSlide>
-                        <SwiperSlide>
-                            <div className="swiper_image_wrapper">
-                                <img src={post.images}/>
-                            </div>
-                        </SwiperSlide>                        
-                    </Swiper>
-                </div>
-                <div className="detail_content_wrapper">
-                    <p>{post.content}</p>
-                </div>
-                
             </div>
             :<></>
             }
