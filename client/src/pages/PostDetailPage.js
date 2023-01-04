4339752f301255ca821d6b6725004e3b0d1b786b
Duhoon <eclip6@naver.com>
2023-01-04 11:33:28 +0900

    feat: 포스트 디테일 페이지 UI 구현 이용자가 커뮤니티에 작성된 리뷰의 상세 내용을 확인할 수 있는 UI를 작성해주었습니다.

--- client/src/pages/PostDetailPage.js
+++ client/src/pages/PostDetailPage.js
@@ -1,6 +1,82 @@
+// modules
+import {useState, useEffect} from "react";
+import { getPostOne } from "../apis/post";
+import {Swiper, SwiperSlide} from "swiper/react";
+
+// apis
+
+
+// css
+import "../assets/css/postdetail.css";
+import "swiper/css";
+
+function convertDate(date){
+    const parsingDate = new Date(date).toLocaleString();
+    return parsingDate;
+}
+
 const PostDetailPage = () => {
+    const [post, setPost] = useState("");
+
+    useEffect(()=>{
+        (async()=>{
+            const result = await getPostOne(1);
+            setPost(result);
+        })();
+    }, []);
+
+    console.log(post);
+
     return(
-        <div>PostDetailPage</div>
+        <div className="container">
+            {post ?
+            <div className="detail_wrapper">
+                <div className="detail_header">
+                    <div className="header_section">
+                        <div className="detail_profile_wrapper">
+                            <div className="profile_frame">
+                                <img src="https://spnimage.edaily.co.kr/images/Photo/files/NP/S/2022/07/PS22072100041.jpg"/>
+                            </div>
+                        </div>
+                        <div className="detail_title_wrapper">
+                            <h1>{post.title}</h1>
+                            <p>{`작성자: ${post.creator} | 조회 수: ${post.views}`}</p>
+                        </div>
+                    </div>
+                    <div className="detail_data_wrapper">
+                        <p>{post.id}</p>
+                        <p>{convertDate(post.created_at)}</p>
+                    </div>
+                </div>
+                <div className="detail_image_wrapper">
+                    <Swiper
+                        slidesPerView={1}
+                    >
+                        <SwiperSlide>
+                            <div className="swiper_image_wrapper">
+                                <img src={post.images}/>
+                            </div>
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <div className="swiper_image_wrapper">
+                                <img src={post.images}/>
+                            </div>
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <div className="swiper_image_wrapper">
+                                <img src={post.images}/>
+                            </div>
+                        </SwiperSlide>                        
+                    </Swiper>
+                </div>
+                <div className="detail_content_wrapper">
+                    <p>{post.content}</p>
+                </div>
+                
+            </div>
+            :<></>
+            }
+        </div> 
     );
 }
 export default PostDetailPage;
