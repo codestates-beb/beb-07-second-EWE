50b2ebcd56d66410da328523b66ba07372c5ae00
SeolDonghun <ssalssi1@gmail.com>
2023-01-06 23:41:06 +0900

    feat:remote ganache migration done

--- server/src/chainUtils/index.js
+++ server/src/chainUtils/index.js
@@ -1,10 +1,17 @@
 const Web3 = require('web3');
 const TokenABI = require('./ABIs/EWEToken.json').abi;
 const NFTABI = require('./ABIs/EWENFT.json').abi;
-require('dotenv').config({ path: '../.env' }); // TODO: remove later
+// require('dotenv').config({ path: '../.env' }); // TODO: remove later
 
-const { WEB3NETWORK, NFT_CA, TOKEN_CA, ADMIN_PK } = process.env;
-const web3Endpoint = 'http://127.0.0.1:8545';
+const { NODE_ENV, WEB3NETWORK, NFT_CA, TOKEN_CA, ADMIN_PK } = process.env;
+let web3Endpoint;
+if (NODE_ENV === 'remote_test') {
+  web3Endpoint = 'http://20.214.190.113:8545';
+} else {
+  web3Endpoint = 'http://127.0.0.1:8545';
+}
+
+console.log({ NODE_ENV, web3Endpoint });
 
 const web3Http = new Web3(new Web3.providers.HttpProvider(web3Endpoint));
 
