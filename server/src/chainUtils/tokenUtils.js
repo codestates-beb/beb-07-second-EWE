d9af93c90ddea72c07472b9ccf59d53e39b0bcea
SeolDonghun <ssalssi1@gmail.com>
2023-01-06 11:46:27 +0900

    feature: join controller prototype done

--- server/src/chainUtils/tokenUtils.js
+++ server/src/chainUtils/tokenUtils.js
@@ -79,70 +79,72 @@ const approveTokenToAdmin = async (from, amount) => {
 
 const spendApprovedToken = async (sender, recipient, amount) => {
   const adminAccount = web3Http.eth.accounts.privateKeyToAccount(ADMIN_PK);
-  try {
-    const result = await tokenContract.methods
-      .transferFrom(sender, recipient, amount)
-      .send({ from: adminAccount.address });
-    return result;
-  } catch (err) {
-    console.error(err);
-    return false;
-  }
   // try {
-  //   const bytedata = await tokenContract.methods
+  //   const result = await tokenContract.methods
   //     .transferFrom(sender, recipient, amount)
-  //     .encodeABI();
-  //   console.log(bytedata);
-  //   const tx = {
-  //     gas: GAS,
-  //     gasPrice: GASPRICE,
-  //     data: bytedata,
-  //   };
-  //   return signAndSendTx(adminAccount, tx);
+  //     .send({ from: adminAccount.address });
+  //   return result;
   // } catch (err) {
   //   console.error(err);
   //   return false;
   // }
+  try {
+    const bytedata = await tokenContract.methods
+      .transferFrom(sender, recipient, amount)
+      .encodeABI();
+    // console.log(bytedata);
+    const tx = {
+      from: adminAccount.address,
+      to: TOKEN_CA,
+      gas: GAS,
+      gasPrice: GASPRICE,
+      data: bytedata,
+    };
+    return signAndSendTx(adminAccount, tx);
+  } catch (err) {
+    console.error(err);
+    return false;
+  }
 };
 
-// (async () => {
-//   console.log('before send eth');
-//   console.log(await web3Http.eth.getBalance(ADMIN_ADDRESS));
-//   console.log(await web3Http.eth.getBalance(USER_ADDRESS));
+const initialTest = async () => {
+  console.log('before send eth');
+  console.log(await web3Http.eth.getBalance(ADMIN_ADDRESS));
+  console.log(await web3Http.eth.getBalance(USER_ADDRESS));
 
-//   await web3Http.eth.sendTransaction({
-//     from: ADMIN_ADDRESS,
-//     to: USER_ADDRESS,
-//     value: '100000000000000000',
-//   });
+  await web3Http.eth.sendTransaction({
+    from: ADMIN_ADDRESS,
+    to: USER_ADDRESS,
+    value: '100000000000000000',
+  });
 
-//   console.log('after send eth');
-//   console.log(await web3Http.eth.getBalance(ADMIN_ADDRESS));
-//   console.log(await web3Http.eth.getBalance(USER_ADDRESS));
+  console.log('after send eth');
+  console.log(await web3Http.eth.getBalance(ADMIN_ADDRESS));
+  console.log(await web3Http.eth.getBalance(USER_ADDRESS));
 
-//   console.log('admin', await getTokenBalance(ADMIN_ADDRESS));
+  console.log('admin', await getTokenBalance(ADMIN_ADDRESS));
 
-//   await transferTokenToUser(USER_ADDRESS, '100000000'); // admin transfer token to user
-//   await approveTokenToAdmin(USER_ADDRESS, '50000000'); // user approve amount to admin
-//   console.log('admin', await getTokenBalance(ADMIN_ADDRESS));
-//   console.log('user1', await getTokenBalance(USER_ADDRESS));
+  await transferTokenToUser(USER_ADDRESS, '100000000'); // admin transfer token to user
+  await approveTokenToAdmin(USER_ADDRESS, '50000000'); // user approve amount to admin
+  console.log('admin', await getTokenBalance(ADMIN_ADDRESS));
+  console.log('user1', await getTokenBalance(USER_ADDRESS));
 
-//   console.log(
-//     'allowance',
-//     await tokenContract.methods.allowance(USER_ADDRESS, ADMIN_ADDRESS).call(),
-//   );
+  console.log(
+    'allowance',
+    await tokenContract.methods.allowance(USER_ADDRESS, ADMIN_ADDRESS).call(),
+  );
 
-//   await spendApprovedToken(
-//     USER_ADDRESS,
-//     '0x34313f816183d2AF84f9fDe94Df9cDdcA8fF6216',
-//     '10000000',
-//   );
-//   console.log('user1', await getTokenBalance(USER_ADDRESS));
-//   console.log(
-//     'user2',
-//     await getTokenBalance('0x34313f816183d2AF84f9fDe94Df9cDdcA8fF6216'),
-//   );
-// })();
+  await spendApprovedToken(
+    USER_ADDRESS,
+    '0x34313f816183d2AF84f9fDe94Df9cDdcA8fF6216',
+    '10000000',
+  );
+  console.log('user1', await getTokenBalance(USER_ADDRESS));
+  console.log(
+    'user2',
+    await getTokenBalance('0x34313f816183d2AF84f9fDe94Df9cDdcA8fF6216'),
+  );
+};
 
 module.exports = {
   getTokenBalance,
