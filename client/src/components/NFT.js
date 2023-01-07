46290b5a3cc5fdde9f590e226e674d94b16e6c81
Russ <yunsubin481@gmail.com>
2023-01-07 20:03:38 +0900

    responsible web improved

--- client/src/components/NFT.js
+++ client/src/components/NFT.js
@@ -1,9 +1,15 @@
 import '../assets/css/asset.css'
+import { useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import {getNftMetadata} from '../apis/nft'
 const NFT = ({nfts}) => {
-    //nft matadata
+
+
+    console.log(nfts)
     return(
-        <Link to='/postdetail' className="asset_container">
+        <Link
+        // to='/postdetail'
+        className="asset_container">
             <div>
                 <div className='user1'>
                 <div className="user_img">
@@ -12,32 +18,32 @@ const NFT = ({nfts}) => {
 
                     <div className="post_title token_id">
                     <h6>
-                    {/* {nfts.token_id} */}
+                    {nfts.token_id}
                     </h6>
                     </div>
                     <div className="post_num id">#
 
-                    {/* {nfts.id} */}
+                    {nfts.id}
                     
                     </div>
                 </div> 
                 <div className='user2'>
                     <div className="creator">
                     
-                    {/* {nfts.owner} */}
+                    {nfts.owner}
                     
                     </div>
                     <div className="contract_account" >
                     
-                    {/* {nfts.contract_account} */}
+                    {nfts.contract_account}
                     
                     </div>
                 </div>
             </div>
             <div className="image" >
-            {/* {nfts.price} */}
+            {nfts.price}
 
-                {/* <img src ={nfts.price}alt="food"></img> */}
+                <img src ={nfts.metadata === undefined || nfts.metadata=== null ? 'https://play-lh.googleusercontent.com/wQiHW5YgQhHmSR_60o9l2lypA9Vn2_hxH0l2X6Iin5lEGTbmfhrZnP8bKywoRGKkJl4' :nfts.metadata.image}alt="food"></img>
             </div>
 
             <div className="comments">
