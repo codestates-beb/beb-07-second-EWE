be1679b87fdb0139b6d6b81438d36437a53e4458
Russ <yunsubin481@gmail.com>
2023-01-13 09:56:30 +0900

    Footer 수정

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -17,19 +17,28 @@ const Footer = () => {
                     <img className='logo'src={require('../assets/image/EWElogo.png')}></img>
                     <h5>윤수빈 프론트엔드, 디자인</h5>
                     <h5>yunsubin481@gmail.com</h5>
-                    <a className='blog' href='https://velog.io/@nft_sb'><h6>Welcome to my Blog</h6></a>
+                    <a className='blog' 
+                    href='https://velog.io/@nft_sb'>
+                    <h6>Welcome to my Blog</h6></a>
                     <br/>
                     <h5>강두훈 프론트엔드, 디자인</h5>
                     <h5>eclip6@ajou.ac.kr</h5>
-                    <a className='blog' href='https://velog.io/@jejualrock'><h6>Welcome to my Blog</h6></a>
-                    <br/>                    <h5>김현태 백엔드, 스마트 컨트랙트</h5>
-                    <h5>hyuntae384@gmail.com</h5>
-                    <a className='blog' href='https://velog.io/@atoye1'><h6>Welcome to my Blog</h6></a>
+                    <a className='blog'
+                    href='https://velog.io/@jejualrock'>
+                    <h6>Welcome to my Blog</h6></a>
                     <br/>  
                     <h5>설동헌 백엔드, 스마트 컨트랙트</h5>
                     <h5>ssalssi1@gmail.com</h5>
-                    <a className='blog' href='https://www.notion.so/
-                    7eb68268711f40619020318efcaeca0c'><h6>Welcome to my Blog</h6></a>
+                    <a className='blog'
+                    href='https://velog.io/@atoye1'>
+                    <h6>Welcome to my Blog</h6></a>
+                    <br/>
+                    <h5>김현태 백엔드, 스마트 컨트랙트</h5>
+                    <h5>hyuntae384@gmail.com</h5>
+                    <a className='blog'
+                    href='https://www.notion.so/
+                    7eb68268711f40619020318efcaeca0c'>
+                    <h6>Welcome to my Blog</h6></a>
                     <br/> 
 
 
